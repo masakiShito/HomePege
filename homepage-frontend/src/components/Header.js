9934cc558c93cbfa89fa3e7540bfc9444d2b59ea
masakiShito <98877813+masakiShito@users.noreply.github.com>
2024-05-28 18:11:16 +0900

    トップ部分の実装完了
    トップ部分の実装が一旦完了したのでコミット

--- homepage-frontend/src/components/Header.js
+++ homepage-frontend/src/components/Header.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import '../assets/styles/header.css';
+import React from "react";
+import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import "../assets/styles/header.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <motion.header
+      className="header"
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="header-content">
         <h1 className="site-title">とあるエンジニアのサイト</h1>
         <nav className="nav-links">
-          <Link to="profileSection" smooth={true} duration={1000} className="nav-link">Profile</Link>
-          <Link to="careerSection" smooth={true} duration={1000} className="nav-link">Career</Link>
-          <Link to="blogSection" smooth={true} duration={1000} className="nav-link">Blog</Link>
+          <Link
+            to="profileSection"
+            smooth={true}
+            duration={1000}
+            className="nav-link"
+          >
+            Profile
+          </Link>
+          <Link
+            to="careerSection"
+            smooth={true}
+            duration={1000}
+            className="nav-link"
+          >
+            Career
+          </Link>
+          <Link
+            to="blogSection"
+            smooth={true}
+            duration={1000}
+            className="nav-link"
+          >
+            Blog
+          </Link>
         </nav>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
--- homepage-frontend/src/containers/HomePage.js
+++ homepage-frontend/src/containers/HomePage.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Element } from 'react-scroll';
-import { motion } from 'framer-motion';
-import Typing from 'react-typing-effect';
-import Header from '../components/Header';
-import Profile from '../components/Profile';
-import Career from '../components/Career';
-import BlogList from '../components/BlogList';
-import Footer from '../components/Footer';
-import '../assets/styles/intro.css';
-import backgroundImage from '../assets/images/background.jpg';
+import React, { useState, useEffect } from "react";
+import { Element, scroller } from "react-scroll";
+import { motion, AnimatePresence } from "framer-motion";
+import Typing from "react-typing-effect";
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+import Career from "../components/Career";
+import BlogList from "../components/BlogList";
+import Footer from "../components/Footer";
+import "../assets/styles/intro.css";
+import backgroundImage from "../assets/images/background.jpg";
 
 const HomePage = () => {
   const [showMainContent, setShowMainContent] = useState(false);
   const [loadingProgress, setLoadingProgress] = useState(0);
+  const [showHeader, setShowHeader] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setLoadingProgress(prev => {
+      setLoadingProgress((prev) => {
         if (prev < 100) {
           return prev + 1;
         } else {
@@ -31,19 +32,52 @@ const HomePage = () => {
     if (loadingProgress === 100) {
       setTimeout(() => {
         setShowMainContent(true);
+        setShowHeader(false); // 初期状態ではヘッダーを非表示
+        setTimeout(() => {
+          scroller.scrollTo("profileSection", {
+            duration: 2000, // スクロールの持続時間を2秒に設定
+            delay: 0,
+            smooth: "easeInOutQuart",
+          });
+        }, 5000); // 5秒後にスクロール
+
+        setTimeout(() => {
+          setShowHeader(true); // スクロール後にヘッダーを表示
+        }, 7000); // スクロール完了後にヘッダーを表示
       }, 1000); // 1秒後にメインコンテンツを表示
     }
   }, [loadingProgress]);
 
+  const handleScroll = () => {
+    if (window.scrollY > window.innerHeight) {
+      setShowHeader(true);
+    } else {
+      setShowHeader(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-      <Header />
+      <AnimatePresence>{showHeader && <Header />}</AnimatePresence>
       {showMainContent ? (
         <div className="container">
-          <motion.div className="intro" style={{ backgroundImage: `url(${backgroundImage})` }} animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1 }}>
+          <motion.div
+            className="intro"
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
             <h1>
-              <Typing 
-                text={['いつかできるから、今日できる']} 
+              <Typing
+                text={["いつかできるから、今日できる"]}
                 speed={100}
                 eraseSpeed={50}
                 typingDelay={500}
@@ -51,30 +85,30 @@ const HomePage = () => {
               />
             </h1>
           </motion.div>
-          
+
           <Element name="profileSection">
             <Profile />
           </Element>
-          
+
           <Element name="careerSection">
             <Career />
           </Element>
-          
+
           <Element name="blogSection">
             <BlogList />
           </Element>
-          
+
           <Footer />
         </div>
       ) : (
         <div className="intro">
-          <h1>ようこそ、masakiのHPへ</h1>
+          <h1>ようこそ、私のHPへ</h1>
           <div className="loading-container">
-            <motion.div 
-              className="loading-bar" 
-              initial={{ width: 0 }} 
-              animate={{ width: `${loadingProgress}%` }} 
-              transition={{ duration: 0.3 }} 
+            <motion.div
+              className="loading-bar"
+              initial={{ width: 0 }}
+              animate={{ width: `${loadingProgress}%` }}
+              transition={{ duration: 0.3 }}
             />
           </div>
           <p className="loading-text">{loadingProgress}%</p>
